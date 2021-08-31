13ecfae16b55669d04df038301ee7dbf99154a64
Tannon Kew <kew@cl.uzh.ch>
2021-08-31 14:41:26 +0200

    assignment 3 en-fr

--- assignments/03/preprocess_data.sh
+++ assignments/03/preprocess_data.sh
@@ -5,7 +5,9 @@ set -e
 
 pwd=`dirname "$(readlink -f "$0")"`
 base=$pwd/../..
-data=$base/data/en-fr/
+src=fr
+tgt=en
+data=$base/data/$tgt-$src/
 
 # change into base directory to ensure paths are valid
 cd $base
@@ -14,29 +16,29 @@ cd $base
 mkdir -p $data/preprocessed/
 
 # normalize and tokenize raw data
-cat $data/raw/train.fr | perl moses_scripts/normalize-punctuation.perl -l fr | perl moses_scripts/tokenizer.perl -l fr -a -q > $data/preprocessed/train.fr.p
-cat $data/raw/train.en | perl moses_scripts/normalize-punctuation.perl -l en | perl moses_scripts/tokenizer.perl -l en -a -q > $data/preprocessed/train.en.p
+cat $data/raw/train.$src | perl moses_scripts/normalize-punctuation.perl -l $src | perl moses_scripts/tokenizer.perl -l $src -a -q > $data/preprocessed/train.$src.p
+cat $data/raw/train.$tgt | perl moses_scripts/normalize-punctuation.perl -l $tgt | perl moses_scripts/tokenizer.perl -l $tgt -a -q > $data/preprocessed/train.$tgt.p
 
 # train truecase models
-perl moses_scripts/train-truecaser.perl --model $data/preprocessed/tm.fr --corpus $data/preprocessed/train.fr.p
-perl moses_scripts/train-truecaser.perl --model $data/preprocessed/tm.en --corpus $data/preprocessed/train.en.p
+perl moses_scripts/train-truecaser.perl --model $data/preprocessed/tm.$src --corpus $data/preprocessed/train.$src.p
+perl moses_scripts/train-truecaser.perl --model $data/preprocessed/tm.$tgt --corpus $data/preprocessed/train.$tgt.p
 
 # apply truecase models to splits
-cat $data/preprocessed/train.fr.p | perl moses_scripts/truecase.perl --model $data/preprocessed/tm.fr > $data/preprocessed/train.fr 
-cat $data/preprocessed/train.en.p | perl moses_scripts/truecase.perl --model $data/preprocessed/tm.en > $data/preprocessed/train.en
+cat $data/preprocessed/train.$src.p | perl moses_scripts/truecase.perl --model $data/preprocessed/tm.$src > $data/preprocessed/train.$src 
+cat $data/preprocessed/train.$tgt.p | perl moses_scripts/truecase.perl --model $data/preprocessed/tm.$tgt > $data/preprocessed/train.$tgt
 
 # prepare remaining splits with learned models
 for split in valid test tiny_train
 do
-    cat $data/raw/$split.fr | perl moses_scripts/normalize-punctuation.perl -l fr | perl moses_scripts/tokenizer.perl -l fr -a -q | perl moses_scripts/truecase.perl --model $data/preprocessed/tm.fr > $data/preprocessed/$split.fr
-    cat $data/raw/$split.en | perl moses_scripts/normalize-punctuation.perl -l en | perl moses_scripts/tokenizer.perl -l en -a -q | perl moses_scripts/truecase.perl --model $data/preprocessed/tm.en > $data/preprocessed/$split.en
+    cat $data/raw/$split.$src | perl moses_scripts/normalize-punctuation.perl -l $src | perl moses_scripts/tokenizer.perl -l $src -a -q | perl moses_scripts/truecase.perl --model $data/preprocessed/tm.$src > $data/preprocessed/$split.$src
+    cat $data/raw/$split.$tgt | perl moses_scripts/normalize-punctuation.perl -l $tgt | perl moses_scripts/tokenizer.perl -l $tgt -a -q | perl moses_scripts/truecase.perl --model $data/preprocessed/tm.$tgt > $data/preprocessed/$split.$tgt
 done
 
 # remove tmp files
-rm $data/preprocessed/train.fr.p
-rm $data/preprocessed/train.en.p
+rm $data/preprocessed/train.$src.p
+rm $data/preprocessed/train.$tgt.p
 
 # preprocess all files for model training
-python preprocess.py --target-lang en --source-lang fr --dest-dir $data/prepared/ --train-prefix $data/preprocessed/train --valid-prefix $data/preprocessed/valid --test-prefix $data/preprocessed/test --tiny-train-prefix $data/preprocessed/tiny_train --threshold-src 1 --threshold-tgt 1 --num-words-src 4000 --num-words-tgt 4000
+python preprocess.py --target-lang $tgt --source-lang $src --dest-dir $data/prepared/ --train-prefix $data/preprocessed/train --valid-prefix $data/preprocessed/valid --test-prefix $data/preprocessed/test --tiny-train-prefix $data/preprocessed/tiny_train --threshold-src 1 --threshold-tgt 1 --num-words-src 4000 --num-words-tgt 4000
 
 echo "done!"
